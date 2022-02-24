fc6b85afb11830375d336d4ea65661c83483b8d4
andy94077 <andy94077@gmail.com>
2022-02-24 16:43:21 +0800

    Delete unused function

--- fiery/utils/mm_obj_evaluation_utils.py
+++ fiery/utils/mm_obj_evaluation_utils.py
@@ -1,54 +1,7 @@
 import numpy as np
 import pyquaternion
-from nuscenes.utils.data_classes import Box as NuScenesBox
-from nuscenes.eval.detection.data_classes import DetectionBox
-
-# mmdet3d
-from mmdet3d.core import Box3DMode
-from mmdet3d.core.bbox import LiDARInstance3DBoxes
-# from mmdet3d.core.bbox import get_box_type
 from fiery.data import NUSCENE_CLASS_NAMES
-
-
-def mm_bbox3d2nusc_box(bboxes, scores, labels, attrs=None, is_ground_truth=False):
-    """Convert detection results to a list of numpy arrays.
-
-    Args:
-        bboxes (torch.Tensor): Bounding boxes with shape of (n, 5).
-        labels (torch.Tensor): Labels with shape of (n, ).
-        scores (torch.Tensor): Scores with shape of (n, ).
-        attrs (torch.Tensor, optional): Attributes with shape of (n, ). \
-            Defaults to None.
-
-    Returns:
-        dict[str, torch.Tensor]: Bounding box results in cpu mode.
-
-            - boxes_3d (torch.Tensor): 3D boxes.
-            - scores (torch.Tensor): Prediction scores.
-            - labels_3d (torch.Tensor): Box labels.
-            - attrs_3d (torch.Tensor, optional): Box attributes.
-
-    """
-    if is_ground_truth is False:
-        result_dict = dict(
-            boxes_3d=bboxes.to('cpu'),
-            scores_3d=scores.cpu(),
-            labels_3d=labels.cpu())
-
-        # result_dict = dict(
-        #     boxes_3d=bboxes,
-        #     scores_3d=scores,
-        #     labels_3d=labels)
-    else:
-        result_dict = dict(
-            boxes_3d=bboxes.to('cpu'),
-            # scores_3d=scores.cpu(),
-            labels_3d=labels.cpu())
-
-    if attrs is not None:
-        result_dict['attrs_3d'] = attrs.cpu()
-
-    return result_dict
+from nuscenes.utils.data_classes import Box as NuScenesBox
 
 
 def output_to_nusc_box(detection, token, is_eval=False):
@@ -99,17 +52,6 @@ def output_to_nusc_box(detection, token, is_eval=False):
             name=NUSCENE_CLASS_NAMES[labels[i]],
             token=token,
         )
-
-        # box = DetectionBox(
-        #     translation=box_gravity_center[i],
-        #     size=box_dims[i],
-        #     rotation=quat,
-        #     # label=labels[i],
-        #     detection_score=scores[i],
-        #     velocity=velocity,
-        #     detection_name=NUSCENE_CLASS_NAMES[labels[i]],
-        #     sample_token=token,
-        # )
 
         box_list.append(box)
     return box_list
